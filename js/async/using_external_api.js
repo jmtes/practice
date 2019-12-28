8f68d8b8722208bcbd4d818a86d8402d475d376c
jmtes <juliennetesoro@gmail.com>
2019-12-27 16:55:59 -0800

    Implement getJokes function

--- js/async/using_external_api.js
+++ js/async/using_external_api.js
@@ -1,1 +1,27 @@
 document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes (ev) {
+  const number = document.querySelector('input[type="number"]').value;
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'http://api.icndb.com/jokes/random/' + number, true);
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      const jokes = JSON.parse(this.responseText).value;
+
+      let output = '';
+
+      jokes.forEach(function (joke) {
+        output += `<li>${joke.joke}</li>`;
+      });
+
+      document.querySelector('.jokes').innerHTML = output;
+    }
+  };
+
+  xhr.send();
+
+  ev.preventDefault();
+}
