1bb1a04d66ea1881f770b1dae2b44200776b2eb6
jmtes <juliennetesoro@gmail.com>
2019-12-29 10:58:40 -0800

    Test POST

--- js/easyhttp2/app.js
+++ js/easyhttp2/app.js
@@ -1,6 +1,18 @@
 const http = new EasyHTTP();
 
-// Get Users from JSON Placeholder
+// Get users from JSON Placeholder
 http.get('https://jsonplaceholder.typicode.com/users')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+// User data
+const data = {
+  name: 'Juno Tesoro',
+  username: 'jutesoro',
+  email: 'jutesoro@gmail.com'
+};
+
+// Create user
+http.post('https://jsonplaceholder.typicode.com/users', data)
   .then(data => console.log(data))
   .catch(err => console.log(err));
