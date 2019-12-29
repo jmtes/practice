9ef88ef1756d4195ee33ea932aed91a2716ee62b
jmtes <juliennetesoro@gmail.com>
2019-12-28 18:56:23 -0800

    Test POST, PUT, and DELETE functions

--- js/easyhttp/app.js
+++ js/easyhttp/app.js
@@ -1,7 +1,49 @@
 const http = new easyHTTP();
 
-// Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function (err, response) {
+// Get posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function (err, response) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Get single post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, response) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Create data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+// Create post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function (err, response) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Update post
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function (err, response) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Delete post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function (err, response) {
   if (err) {
     console.log(err);
   } else {
