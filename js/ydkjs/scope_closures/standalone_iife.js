7275dbc5c67ed6041ac2d93d1f53344c414de254
jmtes <juliennetesoro@gmail.com>
2020-01-21 15:06:14 -0800

    Go over standalone IIFE syntax

---
+++ js/ydkjs/scope_closures/standalone_iife.js
@@ -0,0 +1,11 @@
+// EXAMPLE OF A STANDALONE IIFE
+
+// Outer scope
+
+(function () {
+  // Inner hidden scope
+})();
+
+// More outer scope
+
+// See ./function_scoping.js for an example of an IIFE that's part of another statement!
