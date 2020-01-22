71bacbfd0ce49dbd0367e50e155282e5ac8b33de
jmtes <juliennetesoro@gmail.com>
2020-01-21 16:21:29 -0800

    Add examples Particularly of alternatives to conditional function definition

--- js/ydkjs/scope_closures/function_declarations_in_blocks.js
+++ js/ydkjs/scope_closures/function_declarations_in_blocks.js
@@ -20,16 +20,42 @@
 
 // COMMON USE CASE: DEFINING A FUNCTION ACCORDING TO SOME ENVIRONMENT STATE
 
-if (typeof Array.isArray != "undefined") {
+if (typeof Array.isArray != 'undefined') {
   function isArray (a) {
     return Array.isArray(a);
   }
 } else {
-  function isArray(a) {
-    return Object.prototype.toString.call(a) == "[Object Array]";
+  function isArray (a) {
+    return Object.prototype.toString.call(a) == '[Object Array]';
   }
 }
 
 // It's tempting to structure code this way for performance reasons, since the `typeof Array.isArray` check is only performed once, as oppposed to defining just one `isArray()` and putting the check inside it, where it runs unnecessarily with each call.
 
 // However, allowing a program to define multiple versions of a function always makes it harder to reason about and maintain, so try not to do it.
+
+// Kyle's suggestion is to avoid conditionally defining functions if at all possible. It may be slightly less performant, but this is a better option:
+
+function isArray2 (a) {
+  if (typeof Array.isArray != 'undefined') {
+    return Array.isArray(a);
+  } else {
+    return Object.protoptype.toString.call(a) == '[object Array]';
+  }
+}
+
+// If the performance hit become a critical issue for your app, consider this approach:
+
+var isArray3 = function isArray (a) {
+  return Array.isArray(a);
+};
+
+// Override the definition, if you must
+if (typeof Array.isArray == 'undefined') {
+  isArray3 = function isArray (a) {
+    return Object.prototype.toString.call(a) == '[object Array]';
+  };
+}
+
+// Note that a function EXPRESSION, rather than a declaration, was placed inside the if statement.
+// It's totally fine for function expressions to be inside blocks, just avoid having function declarations in them.
