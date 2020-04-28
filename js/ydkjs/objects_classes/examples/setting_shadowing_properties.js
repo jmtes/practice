7531fa63199225de222e7fd9bfdf4f8006d41649
jmtes <juliennetesoro@gmail.com>
2020-04-28 16:36:07 -0700

    Add notes on property shadowing and [[Put]]

--- js/ydkjs/objects_classes/examples/setting_shadowing_properties.js
+++ js/ydkjs/objects_classes/examples/setting_shadowing_properties.js
@@ -1,3 +1,5 @@
+// EXAMPLE A
+
 var user = {
   name: 'Helgi',
   get location () {
@@ -31,7 +33,7 @@ console.log(user.birthday); // March 5
 
 // If the property IS already present somewhere higher in the chain, nuanced behavior can occur with the assignment.
 
-// EXAMPLES WITH SHADOWING
+// EXAMPLE B - SHADOWING
 
 // If a property name ends up on both an object and at a higher level of the object's prototype chain, the property at the higher level is shadowed by the one on the lower level.
 // This is because the property lookup will always find the property that's lower in the chain first.
@@ -79,3 +81,58 @@ console.log(Object.hasOwnProperty.call(user, 'location')); // true
 console.log(user2); // No location property
 
 console.log(Object.getOwnPropertyDescriptor(user2, 'location')); // undefined
+
+// If you want to shadow a property in Cases 2 and 3, you can't use = assignment.
+// You have to use Object.defineProperty(...).
+
+// USING DEFINEPROPERTY WITH CASE TWO
+
+console.log(user2.birthday); // March 5
+
+Object.defineProperty(user2, 'birthday', {
+  value: 'September 26'
+});
+
+console.log(user2.birthday); // September 26
+
+// USING DEFINEPROPERTY WITH CASE THREE
+
+Object.defineProperty(user2, 'location', {
+  value: 'Plano, TX'
+});
+
+console.log(user2.location); // Plano, TX
+console.log(user2); // Now has location property that is not a getter!
+console.log(Object.getOwnPropertyDescriptor(user2, 'location')); // No longer undefined!
+
+// EXAMPLE C - IMPLICIT SHADOWING
+
+var anotherUser = {
+  age: 36
+};
+
+var myUser = Object.create(anotherUser);
+
+console.log(anotherUser.age); // 36
+console.log(myUser.age); // 36
+
+console.log(anotherUser.hasOwnProperty('age')); // true
+console.log(myUser.hasOwnProperty('age')); // false
+
+myUser.age++; // Implicit shadowing
+
+console.log(anotherUser.age); // 36
+console.log(myUser.age); // 37
+
+// Age property was added to myUser
+console.log(myUser.hasOwnProperty('age')); // true
+
+// It may appear that Line 122 should have just looked up and incremented the anotherUser.age property itself in place.
+// Instead, the ++ operation corresponds with myUser.age += myUser.age + 1 .
+
+// As a result, [[Get]] looked up the age property via [[Prototype]] to get the current value 36 from anotheruser.age.
+// The value was then incremented by 1 and assigned by [[Put]] to a new shadowed property age on myUser.
+
+// If you wanted to increment anotherUser.age, the proper way to have done it would have been anotherUser.age++ .
+
+// Be careful when dealing with delegated properties that you modify!
