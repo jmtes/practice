1cecf3d0f4147ac6b56c29d63ed23711c370b866
jmtes <juliennetesoro@gmail.com>
2020-05-25 22:24:24 -0700

    Go over boxing and natives

--- js/ydkjs/types_grammar/examples/internal_class.js
+++ js/ydkjs/types_grammar/examples/internal_class.js
@@ -18,3 +18,24 @@ console.log(internalClass.call(undefined)); // "[object Undefined]"
 console.log(internalClass.call('abc')); // "[object String"
 console.log(internalClass.call(123)); // "[object Number]"
 console.log(internalClass.call(true)); // "[object Boolean]"
+
+// EXAMPLE D - UNBOXING WITH VALUEOF()
+
+var a = new String('solitude');
+var b = new Number(5);
+var c = new Boolean(false);
+
+console.log(a.valueOf()); // "solitude"
+console.log(b.valueOf()); // 5
+console.log(c.valueOf()); // false
+
+// EXAMPLE E - IMPLICIT UNBOXING
+
+var d = new String('memories');
+var e = d + '';
+
+console.log(typeof d); // object
+console.log(typeof e); // string
+
+console.log(d); // String {"memories"}
+console.log(e); // "memories"
--- js/ydkjs/types_grammar/examples/natives.js
+++ js/ydkjs/types_grammar/examples/natives.js
@@ -13,3 +13,103 @@ console.log(a instanceof Object); // true
 console.log(a); // String {"Hello world"}
 console.log(a.toString()); // "Hello world"
 console.log(Object.prototype.toString.call(a)); // "[object String]"
+
+// EXAMPLE B - USING FUNCTION()
+
+var foo = new Function('a', 'return a * 2');
+
+// The above is equivalent to:
+// foo = function (a) {
+//   return a * 2;
+// };
+
+var b = foo(2);
+
+console.log(b); // 4
+
+// EXAMPLE C - USING REGEXP(...)
+
+var name = 'Kiyoshi';
+var namePattern = new RegExp('\\b(?:' + name + ')+\\b', 'ig');
+
+// The above is equivalent to:
+// namePattern = /\\b(?:Kiyoshi)+\\b/ig;
+
+var someText = 'Bokurano \bKiyoshi\b Temapark';
+
+var matches = someText.match(namePattern);
+console.log(matches); // ["Kiyoshi"]
+
+// EXAMPLE D - DATE.NOW() POLYFILL
+
+if (!Date.now) {
+  Date.now = function () {
+    return (new Date()).getTime();
+  };
+} else {
+  console.log('ES5, baby');
+}
+
+// EXAMPLE E - USING DATE()
+
+// Calling Date() with the new keyword returns a date object:
+var c = new Date();
+
+console.log(c); // Mon May 25 2020 19:55:07 GMT-0700 (Pacific Daylight Time)
+
+console.log(typeof c); // object
+console.log(Object.prototype.toString.call(c)); // "[object Date]"
+
+// Calling it without the new keyword returns a string:
+var d = Date();
+
+console.log(d); // Mon May 25 2020 19:56:03 GMT-0700 (Pacific Daylight Time)
+
+console.log(typeof d); // string
+console.log(Object.prototype.toString.call(d)); // "[object String]"
+
+// EXAMPLE F - USING ERROR()
+
+function bar (x) {
+  if (!x) {
+    throw new Error('x was not provided.');
+  }
+  console.log(x);
+}
+
+bar('Broken Backs and Bad Balloons');
+
+try {
+  bar(null);
+} catch (err) {
+  console.log(err); // Error: x was not provided.
+
+  console.log(err.message); // x was not provided.
+
+  console.log(err.stack); // Same output as Line 85
+
+  console.log(err.toString()); // Error: x was not provided.
+}
+
+// EXAMPLE G - USING PREDEFINED ES6 SYMBOLS
+
+var obj = {};
+
+obj[Symbol.iterator] = function () {
+  // Iterate stuff
+};
+
+console.log(obj); // {Symbol(Symbol.iterator): ƒ}
+
+// EXAMPLE H - MAKING CUSTOM SYMBOLS
+
+var mySym = Symbol('my symbol');
+
+console.log(mySym); // Symbol(my symbol)
+console.log(mySym.toString()); // "Symbol(my symbol"
+console.log(typeof mySym); // symbol
+
+obj = {};
+obj[mySym] = 'season';
+
+console.log(Object.getOwnPropertySymbols(obj)); // [Symbol(my symbol)]
