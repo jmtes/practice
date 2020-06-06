58e5f4984a34f9a8b80daf5daf826bd9c14d233a
jmtes <juliennetesoro@gmail.com>
2020-06-06 11:27:50 -0700

    Query Github API

--- js/react/github_finder/src/App.js
+++ js/react/github_finder/src/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 // import UserItem from './components/users/UserItem';
 import Users from './components/users/Users';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  // Life cycle method that runs after the first time render() is called in a component and the DOM gets updated as a result.
+  // Mounting refers to the actual addition of the DOM elements created by the React component into the DOM.
+  // This method is where you'd want to make HTTP requests to query external APIs!
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
+
+  // This method is invoked every time rerendering happens in a component. It may happen through a state change or a prop change. Rendering returns the elements to be mounted in the DOM.
   render() {
     return (
       <div className='App'>
         <Navbar title='Github Finder' />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
   }
+
+  // The main difference between mounting and rendering is that mounting happens once, but rendering can happen any number of times!
+
+  // Rendering can be thought of as preparing a parcel for delivery, while mounting can be thought of actually delivering the parcel on a doorstep.
 }
 
 export default App;
--- js/react/github_finder/src/components/users/Users.js
+++ js/react/github_finder/src/components/users/Users.js
@@ -2,34 +2,11 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 export class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      },
-      {
-        id: '2',
-        login: 'defunkt',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt'
-      },
-      {
-        id: '38450133',
-        login: 'jmtes',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/38450133?v=4',
-        html_url: 'https://github.com/jmtes'
-      }
-    ]
-  };
-
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
-          <UserItem user={user} />
+        {this.props.users.map(user => (
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
