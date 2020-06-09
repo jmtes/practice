79a663e3ff58a2d0ba2c20f677738401b280ae74
jmtes <juliennetesoro@gmail.com>
2020-06-09 15:26:16 -0700

    Add backend routes

--- js/react/contact-keeper/server.js
+++ js/react/contact-keeper/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 
+const userRoutes = require('./routes/users');
+const contactRoutes = require('./routes/contacts');
+const authRoutes = require('./routes/auth');
+
 const app = express();
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the ContactKeeper API!' })
 );
+
+// Define routes
+app.use('/api/users', userRoutes);
+app.use('/api/contacts', contactRoutes);
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
