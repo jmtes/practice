40884229bf6514c4ccc92f88031561e272a88c4e
jmtes <juliennetesoro@gmail.com>
2020-06-09 14:13:08 -0700

    Create Home and NotFound components

--- js/react/github_finder/src/App.js
+++ js/react/github_finder/src/App.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
-import Search from './components/users/Search';
-import Users from './components/users/Users';
 import User from './components/users/User';
 
 import GithubState from './context/github/GithubState';
@@ -40,18 +40,10 @@ const App = () => {
               <Alert />
               {/* The Switch component will only render the first route that matches/includes the path! It's very handy for nested components! */}
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
