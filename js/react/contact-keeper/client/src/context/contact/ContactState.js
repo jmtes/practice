d2cb4adf4191baac24029bd6f4014f2c5bd836ac
jmtes <juliennetesoro@gmail.com>
2020-06-10 17:17:04 -0700

    Implement contact deletion

--- js/react/contact-keeper/client/src/context/contact/ContactState.js
+++ js/react/contact-keeper/client/src/context/contact/ContactState.js
@@ -58,6 +58,12 @@ const ContactState = props => {
   };
 
   // Delete contact
+  const deleteContact = id => {
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id
+    });
+  };
 
   // Set current contact
 
@@ -73,7 +79,8 @@ const ContactState = props => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
-        addContact
+        addContact,
+        deleteContact
       }}
     >
       {props.children}
