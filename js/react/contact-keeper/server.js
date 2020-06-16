4a33f979213ac16945881c8c4a02d7a90aade274
jmtes <juliennetesoro@gmail.com>
2020-06-16 11:04:14 -0700

    Prepare for deployment

--- js/react/contact-keeper/server.js
+++ js/react/contact-keeper/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const userRoutes = require('./routes/users');
 const contactRoutes = require('./routes/contacts');
@@ -13,14 +14,21 @@ app.use(express.json({ extended: false }));
 // Connect database
 connectDB();
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the ContactKeeper API!' })
-);
-
 // Define routes
 app.use('/api/users', userRoutes);
 app.use('/api/contacts', contactRoutes);
 app.use('/api/auth', authRoutes);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'prod') {
+  // Set static folder, this should be the build folder that React makes for us!
+  app.use(express.static('client/build'));
+
+  // For all routes that are none of those defined above, look inside the current dir, then the client dir, then the build dir, and finally serve the index.html file.
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
