1fe5eaf605779334b9c2e8ee605cfa5d7e499cd3
jmtes <juliennetesoro@gmail.com>
2020-06-18 11:48:35 -0700

    Create add log modal

--- js/react/it-logger/src/App.js
+++ js/react/it-logger/src/App.js
@@ -3,7 +3,9 @@ import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
 import SearchBar from './components/layout/SearchBar';
+import AddBtn from './components/layout/AddBtn';
 import Logs from './components/logs/Logs';
+import AddLogModal from './components/logs/AddLogModal';
 
 const App = () => {
   useEffect(() => {
@@ -15,6 +17,8 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className='container'>
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </Fragment>
