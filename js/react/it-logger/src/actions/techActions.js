dc480c73a3050af798b7809b5822a1e3ca24486b
jmtes <juliennetesoro@gmail.com>
2020-06-20 14:23:57 -0700

    Add delete tech action

--- js/react/it-logger/src/actions/techActions.js
+++ js/react/it-logger/src/actions/techActions.js
@@ -29,6 +29,8 @@ export const getTechs = () => async dispatch => {
 // Add tech to server
 export const addTech = tech => async dispatch => {
   try {
+    setLoading();
+
     const res = await fetch('/techs', {
       method: 'POST',
       body: JSON.stringify(tech),
@@ -41,6 +43,27 @@ export const addTech = tech => async dispatch => {
     dispatch({
       type: ADD_TECH,
       payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText
+    });
+  }
+};
+
+// Delete tech from server
+export const deleteTech = id => async dispatch => {
+  try {
+    setLoading();
+
+    await fetch(`/techs/${id}`, {
+      method: 'DELETE'
+    });
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id
     });
   } catch (err) {
     dispatch({
--- js/react/it-logger/src/reducers/techReducer.js
+++ js/react/it-logger/src/reducers/techReducer.js
@@ -1,6 +1,7 @@
 import {
   GET_TECHS,
   ADD_TECH,
+  DELETE_TECH,
   TECHS_ERROR,
   SET_LOADING
 } from '../actions/types';
@@ -23,6 +24,12 @@ export default (state = initialState, action) => {
       return {
         ...state,
         techs: [...state.techs, action.payload],
+        loading: false
+      };
+    case DELETE_TECH:
+      return {
+        ...state,
+        techs: state.techs.filter(tech => tech.id !== action.payload),
         loading: false
       };
     case TECHS_ERROR:
